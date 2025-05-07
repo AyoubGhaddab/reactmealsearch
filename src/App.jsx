48fde5ed8cc42bb4535f1790e901e09eaa88ded0
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-07 10:18:35 +0200

    add props to card

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,31 @@ import Card from './components/Card'
 import './App.css'
 
 function App() {
-  const { isDarkMode } = useContext(ThemeContext)
+    const { isDarkMode } = useContext(ThemeContext)
 
-  return (
-    <div
-      className={
-        isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-900'
-      }
-    >
-      <Header />
-      <div className='flex justify-center items-center min-h-screen gap-5'>
-           <Card />
-         <Card />
-      </div>
-    </div>
-  ) 
+    return (
+        <div
+            className={
+                isDarkMode
+                    ? 'bg-gray-900 text-white'
+                    : 'bg-gray-100 text-gray-900'
+            }
+        >
+            <Header />
+            <div className="flex justify-center items-center min-h-screen gap-5">
+                <Card
+                    image="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+                    title="Delicious Meal"
+                    description="This is a short description of the meal. It's tasty, healthy, and easy to prepare."
+                />
+                <Card
+                    image="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+                    title="Delicious Meal 2"
+                    description="This is a short description of the meal. It's tasty, healthy, and easy to prepare."
+                />
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,21 @@
 import React from 'react'
-import { useContext } from 'react';
-import { ThemeContext } from '../ThemeContext';
+import { useContext } from 'react'
+import { ThemeContext } from '../ThemeContext'
 
+const Card = ({ image, title, description }) => {
+    const { isDarkMode } = useContext(ThemeContext)
 
-const Card = () => {
-    const { isDarkMode } = useContext(ThemeContext);
-
-  return (
-  <div className={`max-w-sm rounded overflow-hidden shadow-lg ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
-    <img
-      className="w-full"
-      src="https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
-      alt="Meal"
-    />
-    <div className="px-6 py-4">
-      <div className="font-bold text-xl mb-2">Delicious Meal</div>
-      <p className="text-base">
-        This is a short description of the meal. It's tasty, healthy, and easy to prepare.
-      </p>
-    </div>
-  </div>
-  )
+    return (
+        <div
+            className={`max-w-sm rounded overflow-hidden shadow-lg ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}
+        >
+            <img className="w-full" src={image} alt="Meal" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{title}</div>
+                <p className="text-base">{description}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Card
