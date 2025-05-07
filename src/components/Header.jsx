e2175bffa51f73623ddcca99b29b94ced7d7359c
Ayoub <mohamedayoubghaddab@gmail.com>
2025-05-07 20:05:23 +0200

    add emoji

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
             }`}
         >
             <Link to="/" className="text-xl font-bold">
-                Meal Search
+                🍏 Meal Search
             </Link>
             <div className="flex items-center gap-4">
                 <Link to="/favorites" className="text-lg">
